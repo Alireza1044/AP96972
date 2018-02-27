2c35c5e914fe51035f4d9e3be71c05dbb9e17ac3
Alireza1044 <34605487+Alireza1044@users.noreply.github.com>
2018-02-27 23:03:56 +0330

    First Things for HW2

--- HW2/HW2/Program.cs
+++ HW2/HW2/Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace HW2
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            int IntIn ;
+            float Result , Counter = 0;
+            int [] Numbers = new int[8];
+            for (int i = 0; i < 8; i++) {
+                Numbers[i] = int.Parse(Console.ReadLine());
+            }
+            IntIn = int.Parse(Console.ReadLine());
+            while (IntIn != -1){
+                Counter = 0;
+                for (int i = 0; i < 8; i++)
+                {
+                    if (IntIn == Numbers[i])
+                    {
+                        Counter++;
+                    }
+                }
+                Result = Counter / 8;
+                Console.WriteLine(Result);
+                IntIn = int.Parse(Console.ReadLine());
+            }
         }
     }
 }
