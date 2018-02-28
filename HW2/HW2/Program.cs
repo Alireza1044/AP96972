44cc3fad23a30a713abb8cfc504a522b21166040
Alireza1044 <34605487+Alireza1044@users.noreply.github.com>
2018-02-28 15:00:39 +0330

    HW2 done!

--- HW2/HW2/Program.cs
+++ HW2/HW2/Program.cs
@@ -8,28 +8,49 @@ namespace HW2
 {
     public class Program
     {
+        /// <summary>
+        /// first reads the numbers enrtered by user and stores them in an array
+        /// then reads the number
+        /// and then the "Entrance" function counts the probability and returns it
+        /// Writes the result in the terminal
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
-            int IntIn ;
-            float Result , Counter = 0;
-            int [] Numbers = new int[8];
-            for (int i = 0; i < 8; i++) {
-                Numbers[i] = int.Parse(Console.ReadLine());
+            int SelectedNumber;
+            float Counter = 0;
+            int[] Numbers = new int[8];
+            for (int i = 0; i < 8; i++) // reads the entered numbers from user
+            {
+                Numbers[i] = int.Parse(Console.ReadLine()); //array of the entered numbers
             }
-            IntIn = int.Parse(Console.ReadLine());
-            while (IntIn != -1){
-                Counter = 0;
-                for (int i = 0; i < 8; i++)
+            SelectedNumber = int.Parse(Console.ReadLine());
+            while (SelectedNumber != -1) //counts the probability while the entered number is not -1
+            {
+                Console.WriteLine(Entrance(SelectedNumber, Counter, Numbers));
+                SelectedNumber = int.Parse(Console.ReadLine());
+            }
+        }
+        /// <summary>
+        /// counts the probability of the entered number in the list
+        /// </summary>
+        /// <param name="ArrNumbers"> numbers in the array </param>
+        /// <param name="Counter"> repeatative numbers in the entered numbers </param>
+        /// <param name="Numbers"> entered numbers by user </param>
+        /// <returns> the probability of the chosen number </returns>
+        public static float Entrance(int ArrNumbers, float Counter, int[] Numbers)
+        {
+            float Result; // probability
+            Counter = 0;
+            for (int i = 0; i < 8; i++) // counts the repeatative numbers
+            {
+                if (ArrNumbers == Numbers[i])
                 {
-                    if (IntIn == Numbers[i])
-                    {
-                        Counter++;
-                    }
+                    Counter++;
                 }
-                Result = Counter / 8;
-                Console.WriteLine(Result);
-                IntIn = int.Parse(Console.ReadLine());
             }
+            Result = Counter / 8; // counts the probability
+            return Result;
         }
     }
 }
---
+++ HW2/HW2Tests1/ProgramTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HW2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void EnteranceTest()
+        {
+            int[] Test1 = new int[] { 1, 1, 1, 2, 2, 2, 4, 4 };
+            int[] Test2 = new int[] { 8, 8, 8, 8, 8, 8, 8, 8 };
+            int[] Test3 = new int[] { 1, 1, 1, 1, 6, 6, 6, 6 };
+            Assert.AreEqual(0.375, Program.Entrance(1, 0, Test1), 0);
+            Assert.AreEqual(0.25, Program.Entrance(4, 0, Test1), 0);
+            Assert.AreEqual(0, Program.Entrance(1, 0, Test2), 0);
+            Assert.AreEqual(1, Program.Entrance(8, 0, Test2), 0);
+            Assert.AreEqual(0.5, Program.Entrance(6, 0, Test3), 0);
+        }
+    }
+}
