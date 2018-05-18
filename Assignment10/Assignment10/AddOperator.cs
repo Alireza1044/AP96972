586404ac120d7cb45b98dd3137f16386af89f30b
Alireza1044 <34605487+Alireza1044@users.noreply.github.com>
2018-05-18 15:46:20 +0430

    AddOperator done

---
+++ Assignment10/Assignment10/AddOperator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace OOCalculator
+{
+    public class AddOperator : BinaryOperator
+    {
+        public string Symbol;
+        public int LHS;
+        public int RHS;
+
+        public AddOperator(TextReader reader) 
+            :base()
+        {
+            Symbol = reader.ReadLine();
+            LHS = int.Parse(reader.ReadLine());
+            RHS = int.Parse(reader.ReadLine());
+        }
+
+        public override string OperatorSymbol => "+";
+
+        public override double Evaluate() => (LHS+RHS);
+    }
+}
---
+++ Assignment10/Assignment10Tests/AddOperatorTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OOCalculator;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOCalculator.Tests
+{
+    [TestClass()]
+    public class AddOperatorTests
+    {
+        [TestMethod()]
+        public void AddOperatorTest()
+        {
+            StreamWriter writer;
+            StreamReader reader;
+            AddOperator plus;
+
+            using (writer = new StreamWriter(@"writer.txt"))
+            {
+                writer.WriteLine("Add");
+                writer.WriteLine("3");
+                writer.WriteLine("5");
+            }
+            using (reader = new StreamReader(@"writer.txt"))
+            {
+                plus = new AddOperator(reader);
+            }
+            Assert.AreEqual(plus.LHS, 3);
+            Assert.AreEqual(plus.RHS, 5);
+            Assert.AreEqual(plus.Symbol, "Add");
+        }
+
+        [TestMethod()]
+        public void EvaluateTest()
+        {
+            StreamWriter writer;
+            StreamReader reader;
+            AddOperator plus;
+
+            using (writer = new StreamWriter(@"writer.txt"))
+            {
+                writer.WriteLine("Add");
+                writer.WriteLine("3");
+                writer.WriteLine("5");
+            }
+            using (reader = new StreamReader(@"writer.txt"))
+            {
+                plus = new AddOperator(reader);
+            }
+            Assert.AreEqual(8, plus.Evaluate());
+            Assert.AreEqual("+", plus.OperatorSymbol);
+        }
+    }
+}
