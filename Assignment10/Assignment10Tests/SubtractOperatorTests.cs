d8a638c5ff79e301ebdf3c8d6ddd0181071f0cde
Alireza1044 <34605487+Alireza1044@users.noreply.github.com>
2018-05-19 18:17:12 +0430

    added subtract operator tests

---
+++ Assignment10/Assignment10Tests/SubtractOperatorTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OOCalculator;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOCalculator.Tests
+{
+    [TestClass()]
+    public class SubtractOperatorTests
+    {
+        [TestMethod()]
+        public void SubtractOperatorTest()
+        {
+            string path = @"subtracttest.txt";
+            File.WriteAllText(path, "8\n3");
+            SubtractOperator subtract = new SubtractOperator(File.OpenText(path));
+            Assert.AreEqual(subtract.Evaluate(), 5);
+            Assert.AreEqual(subtract.ToString(), "(8-3)");
+        }
+    }
+}
