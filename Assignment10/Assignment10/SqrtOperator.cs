fd403e21c5b1658d050d6694bb96c755cb2ea4be
Alireza1044 <34605487+Alireza1044@users.noreply.github.com>
2018-05-19 18:34:26 +0430

    sqrt operator done!

--- Assignment10/Assignment10/SqrtOperator.cs
+++ Assignment10/Assignment10/SqrtOperator.cs
@@ -6,12 +6,12 @@ namespace OOCalculator
     public class SqrtOperator : UnaryOperator
     {
         public SqrtOperator(TextReader reader)
+            :base(reader)
         {
-            throw new NotImplementedException();
         }
 
-        public override string OperatorSymbol => throw new NotImplementedException();
+        public override string OperatorSymbol => "Sqrt";
 
-        public override double Evaluate() => throw new NotImplementedException();
+        public override double Evaluate() => Math.Pow(Operand.Evaluate(),0.5);
     }
 }
