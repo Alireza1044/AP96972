2a2a2535935de2174278a8c32c75a5d6a856df28
Alireza1044 <34605487+Alireza1044@users.noreply.github.com>
2018-05-19 18:12:22 +0430

    added negate operator tests

---
+++ Assignment10/Assignment10Tests/NegateOperatorTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OOCalculator;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOCalculator.Tests
+{
+    [TestClass()]
+    public class NegateOperatorTests
+    {
+        [TestMethod()]
+        public void NegateOperatorTest()
+        {
+            string path = @"negatetest.txt";
+            File.WriteAllText(path, "9");
+            NegateOperator negate = new NegateOperator(File.OpenText(path));
+            Assert.AreEqual(negate.Evaluate(), -9);
+            Assert.AreEqual(negate.ToString(), "-(9)");
+        }
+    }
+}
