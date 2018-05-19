388a00393c4aa19c6100f7a5e8d6474a370309a7
Alireza1044 <34605487+Alireza1044@users.noreply.github.com>
2018-05-19 18:24:17 +0430

    divide operator tests added

---
+++ Assignment10/Assignment10Tests/DivideOperatorTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OOCalculator;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOCalculator.Tests
+{
+    [TestClass()]
+    public class DivideOperatorTests
+    {
+        [TestMethod()]
+        public void DivideOperatorTest()
+        {
+            string path = @"dividetest.txt";
+            File.WriteAllText(path, "9\n2");
+            DivideOperator divide = new DivideOperator(File.OpenText(path));
+            Assert.AreEqual(divide.Evaluate(), 4.5);
+            Assert.AreEqual(divide.ToString(), "(9 / 2)");
+        }
+    }
+}
