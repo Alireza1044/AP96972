06d0b810c01cd47db994d27c6a2bc532469eaa76
Alireza1044 <34605487+Alireza1044@users.noreply.github.com>
2018-05-20 21:20:20 +0430

    Added xml ccomments

--- Assignment11/Assignment11/Program.cs
+++ Assignment11/Assignment11/Program.cs
@@ -54,46 +54,113 @@ namespace Assignment11
         public static TitanicData ExtensionMethodPlaceHolder(this IEnumerable<TitanicData> data)
             => data.First();
 
+        /// <summary>
+        /// finds the oldest person on the ship
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns> sorts the data by age descending and returns the first one </returns>
         public static TitanicData OldestPerson(this IEnumerable<TitanicData> data)
             => data.OrderByDescending(p => p.Age).First();
 
+        /// <summary>
+        /// finds the oldest female on the ship
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns> filters the data by gender and then sorts them by age and returns the first one </returns>
         public static TitanicData OldestFemale(this IEnumerable<TitanicData> data)
             => data.Where(p => p.IsFemale).OrderByDescending(p => p.Age).First();
 
+        /// <summary>
+        /// counts the total passengers on the ship
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns> total passengers count </returns>
         public static int TotalPassengers(this IEnumerable<TitanicData> data)
             => data.Count();
 
+        /// <summary>
+        /// counts the total number of females on the ship
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns> total number of females </returns>
         public static int TotalFemales(this IEnumerable<TitanicData> data)
             => data.Where(p => p.IsFemale).Count();
 
+        /// <summary>
+        /// counts the total number of survivors
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns> total number of survivors </returns>
         public static int TotalSurvivors(this IEnumerable<TitanicData> data)
             => data.Where(p => p.Survived).Count();
 
+        /// <summary>
+        /// counts the percentage of survivors to total number of passengers
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns> survivors percentage </returns>
         public static double SurvivorsPercentage(this IEnumerable<TitanicData> data)
             => (double)data.Where(p => p.Survived).Count() * 100 / data.Count();
 
+        /// <summary>
+        /// counts the total number of females who have survived
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns> total survived females </returns>
         public static int FemaleSurvivors(this IEnumerable<TitanicData> data)
             => data.Where(p => p.Survived && p.IsFemale).Count();
 
+        /// <summary>
+        /// counts the percentage of females who survived to total number of females
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns> percentage of survived females to all females </returns>
         public static double FemaleSurvivorsPercentage(this IEnumerable<TitanicData> data)
            => (double)data.Where(p => p.Survived && p.IsFemale).Count() * 100 / data.TotalFemales();
 
+        /// <summary>
+        /// counts the percentage of total survived females to total survived passengers
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns> percentage of survived females to all survived passengers </returns>
         public static double SurvivorsIsFemalePercentage(this IEnumerable<TitanicData> data)
             => (double)data.Where(p => p.Survived && p.IsFemale).Count() * 100 / data.Where(p => p.Survived).Count();
 
+        /// <summary>
+        /// counts the percentage of total kids under 10 who have survived to all the kids under 10
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns> percentage of suvived kids under 10 </returns>
         public static double KidsUnder10Survived(this IEnumerable<TitanicData> data)
             => (double)data.Where(p => (p.Age < 10) && p.Survived).Count() * 100 / data.Where(p => (p.Age < 10)).Count();
 
+        /// <summary>
+        /// groups the passengers by survival status and the port they have boarded the ship
+        /// then sorts 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns> the port with most survived passengers </returns>
         public static string PortsWithMostSurvivors(this IEnumerable<TitanicData> data)
             => data.Where(p => p.Survived).GroupBy(p => p.BoardingPort).OrderByDescending(p => p.Count()).First().Key;
 
+        /// <summary>
+        /// groups the passengers by the ports they have boarded the ship
+        /// then creates an anonymous class and counts the percentage of survived passengers in each port
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns> the port with most survived passengers percentage </returns>
         public static string PortsWithMostSurvivorsPercentage(this IEnumerable<TitanicData> data)
         => data.GroupBy(p => p.BoardingPort).Where(p => p.Key != "").Select(x => new
         {
             PassengerCount = (double) x.Where(p => p.Survived).Count() / x.Count(),
             PortName = x.Key,
         }).OrderByDescending(p => p.PassengerCount).First().PortName;
-
+        
+        /// <summary>
+        /// groups the passengers by age/10 and counts the total passengers of each age group
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns> the age group with most passengers </returns>
         public static string MostAgeGroupPassengers(this IEnumerable<TitanicData> data)
             => data.Where(p => p.Age != null).GroupBy(p => p.Age / 10).Select(x => new
             {
@@ -101,6 +168,11 @@ namespace Assignment11
                 GroupAgeRange = $"{(int)x.Key * 10 } to {(int)x.Key * 10 + 9}"
             }).OrderByDescending(p => p.PassengersCount).First().GroupAgeRange;
 
+        /// <summary>
+        /// groups the passengers by age/10 and counts the total survivors of each age group
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns> the age group with most survivors </returns>
         public static string MostAgeGroupSurvivors(this IEnumerable<TitanicData> data)
             => data.Where(p => p.Age != null).GroupBy(p => p.Age / 10).Select(x => new
             {
